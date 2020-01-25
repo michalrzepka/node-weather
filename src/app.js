e2d20eadf885756949bd6623deb1abbce9cbaf93
Michal <micrzepka@gmail.com>
2020-01-25 23:45:07 +0100

    Adding to Heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
 app.use(express.static(publicDirectoryPath))
@@ -84,6 +86,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 }) 
