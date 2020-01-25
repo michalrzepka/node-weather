ed5c1d2497a050ab16ccc35200d1c1d99435cccb
Michal <micrzepka@gmail.com>
2020-01-26 00:05:53 +0100

    Location name localized

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,6 +5,7 @@ const geocode = (address, callback) => {
     const geocodeReq = {
         url: encodeURIComponent(address) + '.json',
         baseUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
+        language: 'pl-PL',
         qs: {
             limit: 3,
             access_token: 'pk.eyJ1IjoibXJ6OTAiLCJhIjoiY2s1azljNHZkMGF1dDNkbGJ4eWs5N2RnaCJ9.Xsx6Tx6RlahToSQ6prEfhw',
